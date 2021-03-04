600ef6c4e2506fdaef580d17795b99a31339c4bb
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-04 14:20:27 -0500

    good ideas for the if X or O wins, but I might try and go back to what I had on the master with more steps

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -10,21 +10,78 @@ import java.util.Arrays;
 public class Board {
     char x = 'X';
     char o = 'O';
+    char hold;
+    /*String threeX = "XXX";
+    String threeO = "OOO";*/
     String winner = "";
     public Board(Character[][] matrix) {
-        //X wins first
-        if (matrix[0][0] == x && matrix[0][1] == x && matrix[0][2] == x) {
-            winner = winner.concat("X");
-        } else if (matrix[1][0] == x && matrix[1][1] == x && matrix[1][2] == x) {
-            winner = winner.concat("X");
-        } else if (matrix[2][0] == x && matrix[2][1] == x && matrix[2][2] == x) {
-            winner = winner.concat("X");
-        } else if (matrix[0][0] == x && matrix[1][0] == x && matrix[2][0] == x) {
-            winner = winner.concat("X");
-        } else {
-            winner = "";
+        //X wins rows
+        if (matrix[0][0] == hold && matrix[0][1] == hold && matrix[0][2] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        } else if (matrix[1][0] == hold && matrix[1][1] == hold && matrix[1][2] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        } else if (matrix[2][0] == hold && matrix[2][1] == hold && matrix[2][2] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        }
+        //X wins columns
+        else if (matrix[0][0] == hold && matrix[1][0] == hold && matrix[2][0] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        } else if (matrix[0][1] == hold && matrix[1][1] == hold && matrix[2][1] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        } else if (matrix[0][2] == hold && matrix[1][2] == hold && matrix[2][2] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        }
+        //X wins diagonal
+        else if (matrix[0][0] == hold && matrix[1][1] == hold && matrix[2][2] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        }else if (matrix[0][2] == hold && matrix[1][1] == hold && matrix[2][0] == hold) {
+            if (hold == x) {
+                winner = winner.concat("X");
+            } else if(hold == o) {
+                winner = winner.concat("O");
+            }
+        }
+        //catches Ties
+        else {
+            winner = winner.concat("Tie");
         }
     }
+    /*public Board(Character[][] matrix) {
+        StringBuilder rowOne = new StringBuilder();
+        rowOne = rowOne.append(matrix[0][0]).append(matrix[0][1]).append(matrix[0][2]);
+        rowOneStr = rowOne.toString();
+        switch (rowOneStr) {
+
+        }
+    }*/
 
     public Boolean isInFavorOfX() {
         boolean xWins = false;
@@ -44,8 +101,9 @@ public class Board {
 
     public Boolean isTie() {
         Boolean tieGame = false;
-        if (winner.equals("")) {
+        if (winner.equals("Tie")) {
             tieGame = true;
+            winner = "";
         }
         return tieGame;
     }
